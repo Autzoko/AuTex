72e00c02b943c08a2d27cd38a9008bab87e08e66
Autzoko <llt020725@163.com>
2023-10-23 21:49:28 +0800

    lexer_defs

--- include/lexer/lexer_defs.h
+++ include/lexer/lexer_defs.h
@@ -5,47 +5,47 @@
 #ifndef AUTEX_LEXER_DEFS_H
 #define AUTEX_LEXER_DEFS_H
 
-/*Reserved Identifiers*/
-#define LX_RSV_VOID      0
-#define LX_RSV_INT       1
-#define LX_RSV_FLOAT     2
-#define LX_RSV_DOUBLE    3
-#define LX_RSV_IF        4
-#define LX_RSV_ELSE      5
-#define LX_RSV_FOR       6
-#define LX_RSV_DO        7
-#define LX_RSV_WHILE     8
+/*Reserved Tokens*/
+#define LX_RSV_INT      101
+#define LX_RSV_FLOAT    102
+#define LX_RSV_DOUBLE   103
+#define LX_RSV_CHAR     104
+#define LX_RSV_VOID     105
+#define LX_RSV_MAIN     106
+#define LX_RSV_IF       107
+#define LX_RSV_ELIF     108
+#define LX_RSV_ELSE     109
+#define LX_RSV_WHILE    110
+#define LX_RSV_FOR      111
+#define LX_RSV_RETURN   112
 
-/*Identifier*/
-#define LX_IDENTIFIER    100
+/*User Identifier*/
+#define LX_USR_IDF      201
 
-/*Operators*/
-#define LX_OPR_ADD       101     // +
-#define LX_OPR_SUB       102     // -
-#define LX_OPR_MUL       103     // *
-#define LX_OPR_DIV       104     // /
-#define LX_OPR_SADD      105     // ++
-#define LX_OPR_SSUB      106     // --
-#define LX_OPR_SHL       107     // <<
-#define LX_OPR_SHR       108     // >>
-#define LX_OPR_ADDEQ     108     // +=
-#define LX_OPR_SUBEQ     109     // -=
-#define LX_OPR_MULEQ     110     // *=
-#define LX_OPR_DIVEQ     111     // /=
-#define LX_OPR_AND       112     // &&
-#define LX_OPR_OR        113     // ||
-#define LX_OPR_NOT       114     // !
+/*Symbols*/
+#define LX_SYM_COMMA    301     // ,
+#define LX_SYM_SMCL     302     // ;
+#define LX_SYM_DOT      303     // .
+#define LX_SYM_LBRC     304     // (
+#define LX_SYM_RBRC     305     // )
+#define LX_SYM_LBRk     306     // [
+#define LX_SYM_RBRK     307     // ]
+#define LX_SYM_LCUR     308     // {
+#define LX_SYM_RCUR     309     // }
+#define LX_SYM_SHARP    310     // #
+#define LX_SYM_AT       311     // @
+#define LX_SYM_SQM      312     // '
+#define LX_SYM_DQm      313     // "
 
-
-
-/*Char*/
-#define LX_CHAR_UPPER    201
-#define LX_CHAR_LOWER    202
-
-/*Exceptions*/
-#define LX_GLB_ERR       -1
-#define LX_CHAR_ERR      -200
-
+/*Single operator*/
+#define LX_SO_ADD       401     // +
+#define LX_SO_SUB       402     // -
+#define LX_SO_MUL       403     // *
+#define LX_SO_DIV       404     // /
+#define LX_SO_AND       405     // &
+#define LX_SO_OR        406     // |
+#define LX_SO_IDIV      407     // %
+#define LX_SO_ASSIGN    408     // :
 
 
 #endif //AUTEX_LEXER_DEFS_H
