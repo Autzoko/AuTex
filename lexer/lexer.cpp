f0c0770fb4269aaab692dc85a4c36ece7b83f347
Autzoko <llt020725@163.com>
2023-10-26 15:10:09 +0800

    slightly optimized

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,10 +1,6 @@
 //
 // Created by llt02 on 10/25/2023.
 //
-
-
-#include <utility>
-
 #include "../include/lexer/lexer.h"
 
 
@@ -15,51 +11,29 @@ lexer::lexer(string file, string target) {
 }
 
 void lexer::analyze() {
-    using NOTE_FLAG = int;
-    using LINE_FLAG = int;
-
     char c = *input.begin();
-    NOTE_FLAG noteFlag = 0;
-    LINE_FLAG lineFlag = 0;
-
 
     for(string::iterator iter = ++input.begin(); iter != input.end(); iter++)
     {
-        if(c == '/' and *iter == '*')
-        {
-            while(c != '*' and *iter != '/')
-            {
+        if(c == '/' and *iter == '*') {
+            while (c != '*' and *iter != '/') {
                 iter++;
                 c = *iter;
             }
             iter += 2;
             c = *iter;
-            continue;
-        }
-
-        if(c == '/' and *iter == '/')
-        {
+        }else if(c == '/' and *iter == '/'){
             while(c != '\n')
             {
                 iter++;
                 c = *iter;
             }
-            continue;
-        }
-        if(c == ' ' or c == '\t')
-        {
+        }else if(c == ' ' or c == '\t'){
             c = *(iter);
-            continue;
-        }
-
-        if(c == '\n')
-        {
+        }else if(c == '\n'){
             tuples_.emplace_back(LX_LINE, "\n");
             c = *(iter);
-            continue;
-        }
-
-        if(lexer_is_letter_(c) == LX_IDF)
+        }else if(lexer_is_letter_(c) == LX_IDF)
         {
             string _tmp;
             _tmp += c;
@@ -72,11 +46,7 @@ void lexer::analyze() {
             }
             LX_TYPE _type = lexer_is_rsv_tokens_(const_cast<char*>(_tmp.c_str()));
             tuples_.emplace_back(_type, _tmp);
-            continue;
-        }
-
-        if(lexer_is_digit_(c) == LX_NUM)
-        {
+        }else if(lexer_is_digit_(c) == LX_NUM){
             string _tmp;
             _tmp += c;
             while(lexer_is_digit_(c) == LX_NUM)
@@ -87,31 +57,19 @@ void lexer::analyze() {
                 _tmp += c;
             }
             tuples_.emplace_back(LX_NUM, _tmp);
-            continue;
-        }
-
-        if(lexer_is_symbols_(c) != LX_SYM_ERR)
-        {
+        }else if(lexer_is_symbols_(c) != LX_SYM_ERR){
             string _tmp;
             _tmp += c;
             tuples_.emplace_back(lexer_is_symbols_(c), _tmp);
             c = *iter;
-            continue;
-        }
-
-        if(lexer_is_single_comparator_(c) != LX_SC_ERR)
-        {
+        }else if(lexer_is_single_comparator_(c) != LX_SC_ERR){
             string _tmp;
             _tmp += c;
             c = *(iter++);
             _tmp += c;
             LX_TYPE _type = lexer_is_double_comparator_(lexer_is_single_comparator_(c), c);
             tuples_.emplace_back(_type, _tmp);
-            continue;
-        }
-
-        if(lexer_is_single_operator_(c) != LX_SO_ERR)
-        {
+        }else if(lexer_is_single_operator_(c) != LX_SO_ERR){
             string _tmp;
             LX_TYPE _type = lexer_is_single_operator_(c);
             _tmp += c;
@@ -125,6 +83,8 @@ void lexer::analyze() {
             _type = lexer_is_double_operator_(_type, c);
             _tmp += c;
             tuples_.emplace_back(_type, _tmp);
+        }else{
+            cerr << "Invalid token." << endl;
         }
     }
 }
